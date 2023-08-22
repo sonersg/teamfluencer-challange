29b185c99817a79a84eb686367a8c844501115b1
Soner <sonersg@outlook.com>
2023-08-22 21:16:13 +0300

    router

---
+++ src/components/UserDetails.tsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+function UserDetailes() {
+    return (
+        <div>
+            UserDetailes
+            <h2>
+                <Link to="/">Home</Link>
+            </h2>
+        </div>
+    );
+}
+
+export default UserDetailes;
